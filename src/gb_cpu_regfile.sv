1481ca72929261888d3f298f4abcca4600ec54e1
Alex Ghandhi <alexpghandhi@gmail.com>
2025-01-26 21:15:04 -0700

    move regfile reads to combinational logic

--- src/gb_cpu_regfile.sv
+++ src/gb_cpu_regfile.sv
@@ -47,7 +47,32 @@ module gb_cpu_regfile (
     assign flags_o.H = f[5];
     assign flags_o.C = f[4];
 
-    // TODO: async reset???
+    // Combinational logic: reads
+    always_comb begin
+        // Handle Reads
+        case (reg8_rd)
+            REG_B:   reg8_o = b;
+            REG_C:   reg8_o = c;
+            REG_D:   reg8_o = d;
+            REG_E:   reg8_o = e;
+            REG_H:   reg8_o = h;
+            REG_L:   reg8_o = l;
+            REG_IE:  reg8_o = ie;
+            REG_IF:  reg8_o = ir;
+            default: reg8_o = 8'hxx;
+        endcase
+        case (reg16_rd)
+            REG_AF:  reg16_o = {a, f};
+            REG_BC:  reg16_o = {b, c};
+            REG_DE:  reg16_o = {d, e};
+            REG_HL:  reg16_o = {h, l};
+            REG_SP:  reg16_o = sp;
+            REG_PC:  reg16_o = pc;
+            default: reg16_o = 16'hxxxx;
+        endcase
+    end
+
+    // Synchronous logic: writes and reset
     always_ff @(posedge clk) begin
         if (reset) begin
             sp <= 16'd0;
@@ -65,29 +90,6 @@ module gb_cpu_regfile (
         end else begin
             // Update Flags
             f <= {flags_i.Z, flags_i.N, flags_i.H, flags_i.C, 4'h0};
-
-            // Handle Reads
-            case (reg8_rd)
-                REG_B:   reg8_o <= b;
-                REG_C:   reg8_o <= c;
-                REG_D:   reg8_o <= d;
-                REG_E:   reg8_o <= e;
-                REG_H:   reg8_o <= h;
-                REG_L:   reg8_o <= l;
-                REG_IE:  reg8_o <= ie;
-                REG_IF:  reg8_o <= ir;
-                default: reg8_o <= 8'hxx;
-            endcase
-            case (reg16_rd)
-                REG_AF:  reg16_o <= {a, f};
-                REG_BC:  reg16_o <= {b, c};
-                REG_DE:  reg16_o <= {d, e};
-                REG_HL:  reg16_o <= {h, l};
-                REG_SP:  reg16_o <= sp;
-                REG_PC:  reg16_o <= pc;
-                default: reg16_o <= 16'hxxxx;
-            endcase
-
             // Handle Writes
             case (reg8_wr)
                 REG_B:  b <= (wren8 ? data8_i : b);
@@ -109,6 +111,5 @@ module gb_cpu_regfile (
             endcase
         end
     end
-
 
 endmodule : gb_cpu_regfile
