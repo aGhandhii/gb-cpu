90f2c0e0e101d070f1795a4f4380afe597299110
Alex Ghandhi <alexpghandhi@gmail.com>
2025-02-03 21:11:29 -0700

    further abstractions for instruction scheduler

--- inc/gb_cpu_common_pkg.sv
+++ inc/gb_cpu_common_pkg.sv
@@ -50,11 +50,10 @@ package gb_cpu_common_pkg;
     } alu_flags_t;
 
     // Used for control signals - specify alu input source
-    // Instruction immediates are considered register sources and
-    // will be stored in the IR register
-    typedef enum logic [2:0] {
+    // the inputs can be from an 8-bit register or the data bus immediate
+    typedef enum logic [1:0] {
         ALU_SRC_REG,
-        ALU_SRC_MEM
+        ALU_SRC_DATA
     } alu_operand_source_t;
 
     // }}}
@@ -74,6 +73,39 @@ package gb_cpu_common_pkg;
 
     // }}}
 
+    // REGISTER FILE {{{
+
+    // IE is memory-mapped to 0xFFFF but is located within the core
+    // IF stores the current instruction
+    typedef enum logic [3:0] {
+        REG_A,
+        REG_F,
+        REG_B,
+        REG_C,
+        REG_D,
+        REG_E,
+        REG_H,
+        REG_L,
+        REG_SP_L,
+        REG_SP_H,
+        REG_PC_L,
+        REG_PC_H,
+        REG_TMP_L,
+        REG_TMP_H
+    } regfile_r8_t;
+
+    typedef enum logic [2:0] {
+        REG_AF,
+        REG_BC,
+        REG_DE,
+        REG_HL,
+        REG_SP,
+        REG_PC,
+        REG_TMP
+    } regfile_r16_t;
+
+    // }}}
+
     // DECODER AND SCHEDULING {{{
 
     typedef enum logic [2:0] {
@@ -89,6 +121,72 @@ package gb_cpu_common_pkg;
         ADDR_BUS_REG8,
         ADDR_BUS_ZERO
     } addr_bus_source_t;
+
+    typedef struct {
+
+        // Address bus can drive one of the following:
+        //  - a 16-bit register value
+        //  - 0xFF00 + an 8 bit register value
+        //  - 0x0000
+        addr_bus_source_t addr_bus_source;
+        regfile_r8_t      addr_bus_source_r8;
+        regfile_r16_t     addr_bus_source_r16;
+
+        // This will synthesize on an FPGA, so data bus is split for I/O
+        regfile_r8_t data_bus_i_destination;  // where to write incoming data on bus
+        regfile_r8_t data_bus_o_source;       // register to drive over data bus
+        logic        drive_data_bus;          // if high, push output - else take input
+
+        idu_opcode_t  idu_opcode;
+        regfile_r16_t idu_operand;
+        logic         idu_wren;
+        regfile_r16_t idu_destination;
+
+        alu_opcode_t         alu_opcode;
+        alu_operand_source_t alu_operand_a_source;
+        alu_operand_source_t alu_operand_b_source;
+        regfile_r8_t         alu_operand_a_register;
+        regfile_r8_t         alu_operand_b_register;
+        logic                alu_inc_dec;             // Pass 1 as operand_b
+        regfile_r8_t         alu_destination;
+        logic                alu_wren;
+        logic                bit_cmd;
+
+        // There are a few additional possible 'miscellaneous operations'
+        // (source)
+        // These include:
+        //  - set/reset IME
+        //  - write contents of 16-bit TMP register to a 16-bit register
+        //  - write the 'restart' address to the Program Counter
+        //  - check a condition code
+        logic enable_interrupts;
+        logic disable_interrupts;
+        logic rst_cmd;
+        logic cc_check;            // check condition code
+        // TODO: enable interrupts with a delay?
+
+        // We also want to signal if an instruction is 0xCB-prefixed
+        // - this signal can be flopped so the next address read will be
+        //   decoded into the correct instruction
+        logic cb_prefix;
+
+    } control_signals_t;
+
+    // Instruction scheduling will have an array of control signals, and the
+    // m-cycle count for the particular instruction.
+    // When new instructions are read, the array will be defined for the next
+    // 6 cycles - enough to account for all commands at any given time, the
+    // 'current' control signals are held in a separate register at top level.
+    // Setting new signals will be done with combinational logic for whatever
+    // the value of the IR register is at a given moment
+    typedef struct {
+        control_signals_t [5:0] instruction_controls;
+        logic [2:0]             m_cycles;
+    } schedule_t;
+
+    // }}}
+
+    // REPURPOSE IN TOP LEVEL - USED TO PARSE INSTRUCTION BITS
 
     typedef enum logic [2:0] {
         r8_b       = 3'o0,
@@ -128,91 +226,5 @@ package gb_cpu_common_pkg;
         cond_nc = 2'b10,
         cond_c  = 2'b11
     } opcode_cond_t;
-
-    // Instruction scheduling will have an array of control signals.
-    // When new instructions are read, the array will be redefined for the next
-    // 6 cycles - enough to account for all commands at any given time, the
-    // 'current' control signals are held in a separate register at top level,
-    // setting new signals will be done with combinational logic
-    typedef struct {
-
-        // Address bus can drive one of the following:
-        //  - a 16-bit register value
-        //  - 0xFF00 + an 8 bit register value
-        //  - 0x0000
-        addr_bus_source_t addr_bus_source;
-        regfile_r8_t      addr_bus_source_r8;
-        regfile_r16_t     addr_bus_source_r16;
-
-        // This will synthesize on an FPGA, so data bus is split for I/O
-        regfile_r8_t data_bus_i_destination;  // where to write incoming data on bus
-        regfile_r8_t data_bus_o_source;       // register to drive over data bus
-        logic        drive_data_bus;          // if high, push output - else take input
-
-        idu_opcode_t  idu_opcode;
-        regfile_r16_t idu_operand;
-        logic         idu_wren;
-        regfile_r16_t idu_destination;
-
-        alu_opcode_t         alu_opcode;
-        alu_operand_source_t alu_operand_a_source;
-        alu_operand_source_t alu_operand_b_source;
-        regfile_r8_t         alu_operand_a_register;
-        regfile_r8_t         alu_operand_b_register;
-        logic                alu_inc_dec;             // Pass 1 as operand_b
-        regfile_r8_t         alu_destination;
-        logic                alu_wren;
-        logic                bit_cmd;
-        logic [7:0]          bit_index;
-
-        // There are a few additional possible 'miscellaneous operations'
-        // (source)
-        // These include:
-        //  - set/reset IME
-        //  - write contents of 16-bit TMP register to a 16-bit register
-        //  - write the 'restart' address to the Program Counter
-        //  - check a condition code
-        logic       enable_interrupts;
-        logic       disable_interrupts;
-        logic       rst_cmd;
-        logic [7:0] rst_target_addr;     // for restart command
-        logic       cc_check;            // check condition code
-        // TODO: enable interrupts with a delay?
-
-    } control_signals_t;
-
-    // }}}
-
-    // REGISTER FILE {{{
-
-    // IR/IE are memory-mapped but are located within the core
-    typedef enum logic [3:0] {
-        REG_A,
-        REG_F,
-        REG_B,
-        REG_C,
-        REG_D,
-        REG_E,
-        REG_H,
-        REG_L,
-        REG_SP_L,
-        REG_SP_H,
-        REG_PC_L,
-        REG_PC_H,
-        REG_TMP_L,
-        REG_TMP_H
-    } regfile_r8_t;
-
-    typedef enum logic [2:0] {
-        REG_AF,
-        REG_BC,
-        REG_DE,
-        REG_HL,
-        REG_SP,
-        REG_PC,
-        REG_TMP
-    } regfile_r16_t;
-
-    // }}}
 
 endpackage : gb_cpu_common_pkg
